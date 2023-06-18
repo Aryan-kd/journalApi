67b0c9f2fb289b14a3955feb5511e5d86d4ed695
Aryan Kodinya <aryan.kodinya@gmail.com>
2023-06-18 13:10:55 +0530

    Index.ejs updated

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.get('/', (req, res) => {
 // @route POST /upload
 // @desc  Uploads file to DB
 app.post('/upload', upload.single('file'), (req, res) => {
-  res.json({ file: req.file });
+  // res.json({ file: req.file });
   res.redirect('/');
 });
 
